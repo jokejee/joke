3ec536218598c05fabb2bfcf9bc3da0979b6ab72
jokejee <jokejee29@gmail.com>
2023-10-04 15:16:26 +0700

    ten

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'ธนพนธ์ อินท์ขะ',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
